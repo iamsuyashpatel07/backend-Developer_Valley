093c5f204520e2f4e97e5cc812c97ad7e651ad0d
[suyash patel] <suyashpatelmrj@gmail.com>
2023-01-01 15:23:06 +0530

    degraded  react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import { Fragment } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Landing } from './components/layout/Landing';
 import { Navbar } from './components/layout/Navbar';
+import { Register } from './components/auth/Register';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route path='/register' component={Register} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
